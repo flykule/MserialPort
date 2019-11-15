dc6918248db2a2016e07dc106f4b317be52b209e
flykule <Casltliu40@gmail.com>
2019-11-15 10:30:26 +0800

    修复找不到库的错误

--- app/src/main/java/com/example/mserialport/MainActivity.kt
+++ app/src/main/java/com/example/mserialport/MainActivity.kt
@@ -72,17 +72,7 @@ class MainActivity : AppCompatActivity() {
      */
 //    external fun stringFromJNI(): String
 
-    external fun testSendData(data: String)
-
-//    external fun openFD(): FileDescriptor?
-
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
-    }
+//    external fun testSendData(data: String)
 
     fun enUnicode(str: String): String {// 将汉字转换为16进制数
 
